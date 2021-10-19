2c4977bb542c545f1d990e872ec200d90323c7d4
vinceh121 <contact@vinceh121.me>
2021-10-19 18:23:02 +0200

    fix server side events

---
+++ 1.16/src/main/java/me/vinceh121/minegasm/mixin/ClientPlayerEntityMixins.java
@@ -0,0 +1,34 @@
+package me.vinceh121.minegasm.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.therainbowville.minegasm.client.ClientEventHandler;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixins {
+	@Inject(at = @At("HEAD"), method = "setExperience(FII)V")
+	private void onXpAdded(float progress, int total, int level, CallbackInfo ci) {
+		ClientPlayerEntity thos = (ClientPlayerEntity) (Object) this;
+		ClientEventHandler.onXpChange(thos, level - thos.experienceLevel);
+	}
+
+	@Inject(at = @At("HEAD"), method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)V")
+	private void playerReceivedDamage(DamageSource source, float health, CallbackInfoReturnable<Boolean> ci) {
+		ClientEventHandler.onHurt(((ClientPlayerEntity) (Object) this).getGameProfile());
+	}
+
+	@Inject(at = @At("INVOKE"), method = "updateHealth(F)V")
+	private void onGoCommitDie(float health, CallbackInfo ci) {
+		if (health == 0) {
+			ClientEventHandler.onDeath((PlayerEntity) (Object) this);
+		}
+	}
+}
--- 1.16/src/main/java/me/vinceh121/minegasm/mixin/PlayerEntityMixins.java
+++ 1.16/src/main/java/me/vinceh121/minegasm/mixin/PlayerEntityMixins.java
@@ -1,46 +1,25 @@
 package me.vinceh121.minegasm.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.mojang.authlib.GameProfile;
 import com.therainbowville.minegasm.client.ClientEventHandler;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixins {
-	@Shadow
-	public abstract GameProfile getGameProfile();
-
-	@Inject(at = @At("INVOKE"), method = "applyDamage(Lnet/minecraft/entity/damage/DamageSourceF;)V")
-	private void playerReceivedDamage(DamageSource source, float health, CallbackInfo ci) {
-		ClientEventHandler.onHurt(getGameProfile());
-	}
-
 	@Inject(at = @At("TAIL"), method = "tick()V")
 	private void playerTick(CallbackInfo ci) {
 		ClientEventHandler.onPlayerTick((PlayerEntity) (Object) this);
 	}
 
-	@Inject(at = @At("INVOKE"), method = "onDeath(Lnet/minecraft/entity/damage/DamageSource;)V")
-	private void onGoCommitDie(DamageSource source, CallbackInfo ci) {
-		ClientEventHandler.onDeath((PlayerEntity) (Object) this);
-	}
-
 	@Inject(at = @At("RETURN"), method = "canHarvest(Lnet/minecraft/block/BlockState;)Z")
 	private void onHarvestCheck(BlockState state, CallbackInfoReturnable<Boolean> ci) {
 		ClientEventHandler.onHarvest((PlayerEntity) (Object) this, state, ci.getReturnValueZ());
-	}
-
-	@Inject(at = @At("INVOKE"), method = "addExperience(I)V")
-	private void onXpAdded(int xpChange, CallbackInfo ci) {
-		ClientEventHandler.onXpChange((PlayerEntity) (Object) this, xpChange);
 	}
 }
