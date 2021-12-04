137e1f47a43aded67590c51ccce25ea3a2354134
vinceh121 <contact@vinceh121.me>
2021-12-04 16:33:52 +0100

    Begin 1.18 port

--- src/main/java/me/vinceh121/minegasm/mixin/ClientWorldMixins.java
+++ src/main/java/me/vinceh121/minegasm/mixin/ClientWorldMixins.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.therainbowville.minegasm.client.ClientEventHandler;
 
@@ -21,14 +22,14 @@ import net.minecraft.world.dimension.DimensionType;
 @Mixin(ClientWorld.class)
 public class ClientWorldMixins {
 	@Inject(at = @At("TAIL"), method = "<init>()V")
-	public void onWorldLoad(ClientPlayNetworkHandler clientPlayNetworkHandler, ClientWorld.Properties properties,
-			RegistryKey<World> registryKey, DimensionType dimensionType, int i, Supplier<Profiler> supplier,
-			WorldRenderer worldRenderer, boolean bl, long l, CallbackInfo ci) {
+	public void onWorldLoad(ClientPlayNetworkHandler netHandler, ClientWorld.Properties properties,
+			RegistryKey<World> registryRef, DimensionType dimensionType, int loadDistance, int simulationDistance,
+			Supplier<Profiler> profiler, WorldRenderer worldRenderer, boolean debugWorld, long seed, CallbackInfo ci) {
 		ClientEventHandler.onWorldLoaded((World) (Object) this);
 	}
 
-	@Inject(at = @At("HEAD"), method = "finishRemovingEntity")
-	public void onRemoveEntity(Entity entity, CallbackInfo ci) {
+	@Inject(at = @At("TAIL"), method = "removeEntity", locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+	public void onRemoveEntity(int entityId, Entity.RemovalReason removalReason, CallbackInfo ci, Entity entity) {
 		ClientEventHandler.onWorldExit(entity);
 	}
 
