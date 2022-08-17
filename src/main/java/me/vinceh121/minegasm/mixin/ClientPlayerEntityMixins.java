f1211eb5eddad7ce0ef4b23a927718eff1543527
vinceh121 <contact@vinceh121.me>
2022-08-18 01:18:15 +0200

    chore: useless import

--- src/main/java/me/vinceh121/minegasm/mixin/ClientPlayerEntityMixins.java
+++ src/main/java/me/vinceh121/minegasm/mixin/ClientPlayerEntityMixins.java
@@ -1,6 +1,5 @@
 package me.vinceh121.minegasm.mixin;
 
-import com.therainbowville.minegasm.common.Minegasm;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
