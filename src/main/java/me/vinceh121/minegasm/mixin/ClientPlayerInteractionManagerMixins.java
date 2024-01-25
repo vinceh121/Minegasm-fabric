95d8e6e2cf4de6a6bff896894923d851fd5b4537
vinceh121 <contact@vinceh121.me>
2024-01-25 22:00:40 +0100

    fix: harvest hook

--- src/main/java/me/vinceh121/minegasm/mixin/ClientPlayerInteractionManagerMixins.java
+++ src/main/java/me/vinceh121/minegasm/mixin/ClientPlayerInteractionManagerMixins.java
@@ -13,6 +13,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixins {
@@ -22,7 +23,14 @@ public class ClientPlayerInteractionManagerMixins {
 
 	@Inject(method = "breakBlock(Lnet/minecraft/util/math/BlockPos;)Z", at = @At("HEAD"))
 	private void onBreak(BlockPos pos, CallbackInfoReturnable<Boolean> ci) {
-		BlockState state = client.world.getBlockState(pos);
+		BlockState state = this.client.world.getBlockState(pos);
 		ClientEventHandler.onBreak(client.player, state);
+	}
+
+	@Inject(at = @At("RETURN"), method = "updateBlockBreakingProgress(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Z")
+	private void onHarvestCheck(BlockPos pos, Direction dic, CallbackInfoReturnable<Boolean> ci) {
+		BlockState state = this.client.world.getBlockState(pos);
+
+		ClientEventHandler.onHarvest(this.client.player, state, this.client.player.canHarvest(state));
 	}
 }
